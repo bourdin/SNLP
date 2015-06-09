79d5098e20b17528d4ff477df4b46143fa587999
Ata Mesgarnejad <a.mesgarnejad@gmail.com>
2015-06-09 12:23:18 -0500

    untangled make from PETSc if it doesn't exist

--- checkgit.py
+++ checkgit.py
@@ -7,12 +7,15 @@ def getcurrentrevision(code_dir):
 		(stdout, stderr) = gitproc.communicate()
 		gitversion = open(os.path.join(code_dir,"src/gitversion.c"), "w")
 		gitversion.write('''const char *gitversion = "%s";\n'''%stdout.rstrip())
-		petscdir = os.environ['PETSC_DIR']
-		if (os.path.isdir(os.path.join(petscdir,".git"))):
-			os.chdir(code_dir)
-			gitproc = Popen(['git', 'rev-parse','HEAD'], stdout = PIPE)
-			(stdout, stderr) = gitproc.communicate()
-			gitversion.write('''const char *petscgitversion ="%s";\n'''%stdout.rstrip())
+		try:
+			petscdir = os.environ['PETSC_DIR']
+			if (os.path.isdir(os.path.join(petscdir,".git"))):
+				os.chdir(code_dir)
+				gitproc = Popen(['git', 'rev-parse','HEAD'], stdout = PIPE)
+				(stdout, stderr) = gitproc.communicate()
+				gitversion.write('''const char *petscgitversion ="%s";\n'''%stdout.rstrip())
+		except KeyError:
+			gitversion.write('''const char *petscgitversion ="na";\n''')
 		gitversion.close()
 
 if __name__ == "__main__":
